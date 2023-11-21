26b361bb21c8be0d0c58f488aa8d311e5ff16890
SW ENG <sweng@massload.com>
2023-11-21 09:39:17 -0600

    forced gitignore

---
+++ CleanAppFilesGenerator/GenerateCQRSQueryClass.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Text;
+using System.Xml.Linq;
+
+namespace CleanAppFilesGenerator
+{
+    public class GenerateCQRSQueryClass
+    {
+
+
+        public static string GenerateCQRSQuey(Type type, string name_space, Func<string, string, string> produceheader)
+        {
+            var Output = new StringBuilder();
+            Output.Append(produceheader(name_space, type.Name));
+            Output.Append(GeneralClass.newlinepad(0) + GeneralClass.ProduceClosingBrace());
+            return Output.ToString();
+        }
+
+        public static string ProduceGetQueryHeader(string name_space, string entityName)
+        {
+            return ($"using {name_space}.Application.Contracts.RequestDTO;\n" +
+                $"using {name_space}.Application.Contracts.ResponseDTO;\n" +        
+               $"using {name_space}.Domain.Errors;\nusing LanguageExt;\nusing MediatR;\n" +
+               $"namespace {name_space}.Application.CQRS.{entityName}.Queries\n{{{GeneralClass.newlinepad(4)}public  record Get{entityName}Query(ApplicationRequest{entityName}DTO  Request{entityName}DTO) :  IRequest<Either<GeneralFailures, ApplicationResponse{entityName}DTO>>;");
+
+        }
+        public static string ProduceGetAllQueryHeader(string name_space, string entityName)
+        {
+            return ($"using {name_space}.Application.Contracts.RequestDTO;\n" +
+              $"using {name_space}.Application.Contracts.ResponseDTO;\n" +
+             $"using {name_space}.Domain.Errors;\nusing LanguageExt;\nusing MediatR;\n" +
+            // $"namespace {name_space}.Application.CQRS.{entityName}.Queries\n{{{GeneralClass.newlinepad(4)}public  record GetAll{entityName}Query(ApplicationRequest{entityName}DTO : Request{entityName}DTO) :  IRequest<Either<GeneralFailures, IEnumerable<ApplicationResponse{entityName}DTO>>>;");
+              $"namespace {name_space}.Application.CQRS.{entityName}.Queries\n{{{GeneralClass.newlinepad(4)}public  record GetAll{entityName}Query(ApplicationRequest{entityName}DTO  Request{entityName}DTO) :  IRequest<Either<GeneralFailures, IEnumerable<ApplicationResponse{entityName}DTO>>>;");
+
+
+        }
+    }
+}
---
+++ CleanAppFilesGenerator/GenerateEntityClass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CleanAppFilesGenerator
+{
+    public class GenerateEntityClass
+    {
+        public static string GenerateEntity(Type type, string name_space)
+        {
+            string baseEntity = " : BaseEntity";
+            var entityName = type.Name;
+            var Output = new StringBuilder();
+            Output.Append(ProduceEntityHeader(name_space, type, baseEntity));
+            Output.Append(ProduceEntityProperties(type));
+            Output.Append(GeneralClass.newlinepad(8) + GeneralClass.ProduceClosingBrace());
+            Output.Append(GeneralClass.newlinepad(0) + GeneralClass.ProduceClosingBrace());
+            return Output.ToString();
+        }
+
+        public static string ProduceEntityHeader(string name_space, Type type, string baseEntity = " : BaseEntity")
+        {
+
+
+            if (type.BaseType.Name != "Object")
+            {
+                baseEntity = $" : {type.BaseType.Name}";
+            }
+
+            return ($"using {name_space}.DomainBase.Base;\nnamespace {name_space}.Domain.Entities\n{{{GeneralClass.newlinepad(4)}public  class {type.Name} {baseEntity}{GeneralClass.newlinepad(8)}{{");
+            //return ($"using {name_space}.DomainBase.Base;\nnamespace {name_space}.Domain.BaseModels.Entities\n{{{GeneralClass.newlinepad(4)}public  class {entityName} {baseEntity}{GeneralClass.newlinepad(8)}{{");
+
+        }
+
+
+        public static string ProduceEntityProperties(Type type)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo prop in properties)
+            {
+                var x = Nullable.GetUnderlyingType(prop.PropertyType);
+                var propertytype = x == null ? prop.PropertyType.Name : x.Name;
+
+                if (propertytype.Contains("ICollection`1"))
+                {
+                    var xx = prop.PropertyType.GenericTypeArguments[0];
+                    sb.Append(GeneralClass.PreparePropertyAsCollection(xx.Name, prop.Name));
+
+                }
+                else
+
+                    sb.Append(GeneralClass.PrepareProperty(propertytype, prop.Name));
+            }
+
+            return sb.ToString();
+        }
+
+        public static string GenerateBaseEntity(Type type, string name_space)
+        {
+            string baseEntity = "";
+            var entityName = "BaseEntity"; // find the base entity name
+            var Output = new StringBuilder();
+            Output.Append(ProduceEntityHeader(name_space, type, baseEntity));
+            Output.Append(GeneralClass.newlinepad(12) + "public Guid GuidId { get; set; } = default;");
+            Output.Append(GeneralClass.newlinepad(8) + GeneralClass.ProduceClosingBrace());
+            Output.Append(GeneralClass.newlinepad(0) + GeneralClass.ProduceClosingBrace());
+            return Output.ToString();
+        }
+
+
+    }
+
+
+}
+
+
